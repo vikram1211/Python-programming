4255ad2a9982d12db8a34e7debe41165de6192e0
vikram1211 <danduvikram74@gmail.com>
2023-11-06 16:59:08 +0530

    new file:   Leetcode- Two Sum.py 	new file:   helloworld.py 	deleted:    helloworld.py

---
+++ Leetcode- Two Sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums, target):
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    x=[i,j]
+                    return x
+                    break
+                
+            
+s=Solution()
+input_string=input("list of numbers ")
+# break_string=input_string.split()
+nums=list(input_string)
+#  nums=[int(num) for num in break_string]
+target=int(input("provide the sum of two numbers "))
+# result=s.twoSum(nums,target)
+# print("here is the indices of the numbers ",result)
+print(nums)
